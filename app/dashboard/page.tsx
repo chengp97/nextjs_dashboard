b945e8f305e0ecf73085b0bbd35741048dfb863c
cp97 <1978438017@qq.com>
2025-05-14 16:05:56 +0800

    Refactor code structure for improved readability and maintainability

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -50,7 +50,7 @@ export default function DashboardPage() {
             ) : (
                 <p>Loading user information...</p>
             )}
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
                 </Suspense>
@@ -62,7 +62,7 @@ export default function DashboardPage() {
                 <Suspense fallback={<LatestInvoicesSkeleton />}>
                     <LatestInvoices />
                 </Suspense>
-            </div>
+            </div> */}
         </main>
     );
 }
